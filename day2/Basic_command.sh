b6cd3b9c46d5ef32f115fda1158762f7c5139107
rishu <rishuprasad220204@gmail.com>
2023-12-21 22:11:09 +0530

    day-2

---
+++ day2/Basic_command.sh
@@ -0,0 +1,8 @@
+# ls COMMAND 
+ls 
+# pwd COMMAND 
+pwd
+# echo COMMAND 
+echo -e "\nhello world\n"
+# cat COMMAND
+cat Basic_Linux_Command.txt
---
+++ day2/directory_commands.sh
@@ -0,0 +1,16 @@
+
+# mkdir command
+mkdir example_directory1
+
+mkdir -p example_directory1/example_directory2/example_directory3
+echo ""
+ls
+# rmdir command
+echo ""
+rmdir example_directory1
+ls
+#rm -rf command
+echo ""
+rm -rf example_directory1
+ls
+
