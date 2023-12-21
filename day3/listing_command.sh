2e8480788f21effda4a453486bca0b072ccc7965
rishu <rishuprasad220204@gmail.com>
2023-12-21 22:42:36 +0530

    day-3

---
+++ day3/listing_command.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#ls -l command
+ls -l
+echo ""
+
+#ls -a command
+ls -a
+echo ""
+
+#ls -F command
+ls -F
+echo ""
+
+#ls -t command
+ls -t
+echo ""
+
+#ls -R command
+ls -R
+echo ""
+
+#ls --color command
+ls --color 
+echo ""
